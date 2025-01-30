67827c65e0d4b4c5dc051f61beac61cad52510d2
Garv Gupta <120120156+GGarv@users.noreply.github.com>
2025-01-30 09:37:21 +0530

    Updated Code (Fixed issues)
    1. Calculate the square root of "x" with Newton Method.
    2. Calculate the square root of "x" with Inbuilt Method.

--- HSPC_1GG/srt.py
+++ HSPC_1GG/srt.py
@@ -1,9 +1,8 @@
 s=1.0
 x=6.0
 for k in range(20):
-	s=0.5(s+x/s)
+	s=0.5*(s+x/s)
 print("Using Newton's method: ",s)
 
 from math import sqrt
-print("Using inbuilt sqrt fun: ".sqrt(x))
-
+print("Using inbuilt sqrt fun: ",sqrt(x))
